c1549391e8d94a041f4db9a3a7b915d0c6bc7644
gabriel michinhote <gabriel.michinhote@gmail.com>
2026-02-01 17:32:21 -0300

    inicia jogo

--- funcoes.py
+++ funcoes.py
@@ -7,3 +7,19 @@ def cria_pecas():
             pecas.append([i, j])
     random.shuffle(pecas)
     return pecas
+
+
+def inicia_jogo(num_jogadores, pecas):
+    jogo = {
+        'jogadores': {},
+        'monte': [],
+        'mesa': []
+    }
+    pecas_por_jogador = 7
+    for i in range(num_jogadores):
+        inicio = i * pecas_por_jogador
+        fim = inicio + pecas_por_jogador
+        jogo['jogadores'][i] = pecas[inicio:fim]
+    total_distribuidas = num_jogadores * pecas_por_jogador
+    jogo['monte'] = pecas[total_distribuidas:]
+    return jogo
